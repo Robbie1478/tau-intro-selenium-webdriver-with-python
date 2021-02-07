95cf074c38441d6ba730ec6807b5a950f6681a7c
Robert Elliott <Robert.Elliott@emishealth.com>
2021-02-07 08:33:21 +0000

    Chapter 5

--- pages/search.py
+++ pages/search.py
@@ -2,7 +2,13 @@
 This module containt DuckDuckGoSearchPage, the page object for the DuckDuckGo search page.
 """
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 class DuckDuckGoSearchPage:
+
+    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+    RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
 
     def __init__(self, browser):
         self.browser = browser
@@ -14,6 +20,3 @@ class DuckDuckGoSearchPage:
     def search(self, phrase):
         # TODO
         pass
-
-    
-        
